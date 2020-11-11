41a72c9c70b5124c5e298f33bd458a70f9658a0e
riccardo amici <amici95@gmail.com>
2020-11-11 23:27:50 +0100

    sec ex

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all if Rails.env.development?
+puts "Creating restaurants..."
+raikou = { name: "Raikou", address: "7 Boundary St, London E2 7JE", category: "italian" }
+entei =  { name: "Entei", address: "56A Shoreditch High St, London E1 6PQ", category: "french" }
+suicune = { name: "Suicune", address: "7 Boundary St, London E2 7JE", category: "japanese" }
+hooh = { name: "Ho-oh", address: "7 Boundary St, London E2 7JE", category: "italian" }
+lugia = { name: "Lugia", address: "55 Kuglerstr., Berlin E2 7JE", category: "japanese" }
+[raikou, entei, suicune, hooh, lugia].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
